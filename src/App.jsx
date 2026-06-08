705e0365b3b602c3538cdce772877d229aa9ccc3
dbtu <nazarhayidov@gmail.com>
2026-06-08 19:06:18 +0300

    feat(cards): add cards table with status badges and action icons

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import Form from "./components/Form/Form";
 import Header from "./components/Header/Header";
 import * as styles from "./App.module.css";
 import Footer from "./components/Footer/Footer";
+import Cards from "./components/Cards/Cards";
 
-function App() {
+export default function App() {
   return (
     <div className={styles.body}>
       <Header />
       <main className={styles.main}>
         <h1>Merchant agreement</h1>
         <Form />
+        <Cards />
       </main>
       <Footer />
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Cards/GoToIcon.jsx
@@ -0,0 +1,15 @@
+export default function GoToIcon() {
+  return (
+    <svg
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g>
+        <path d="M9 16.5C4.85775 16.5 1.5 13.1422 1.5 9C1.5 4.85775 4.85775 1.5 9 1.5C13.1422 1.5 16.5 4.85775 16.5 9C16.5 13.1422 13.1422 16.5 9 16.5ZM12.75 12L16.5 9L12.75 6V8.25H6.75V9.75H12.75V12Z" />
+      </g>
+    </svg>
+  );
+}
