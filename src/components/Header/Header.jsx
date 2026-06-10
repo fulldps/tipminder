f099584d97f2214ba9d3266b489f823a4b46c4af
dbtu <nazarhayidov@gmail.com>
2026-06-10 19:17:09 +0300

    feat(header): add logo/user icons and styled logout button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,8 @@
+import TipMinderIcon from "./TipMinderIcon";
+import UserIcon from "./UserIcon";
+
 import * as styles from "./Header.module.css";
+
 const NAV_MENU_ITEMS = [
   { id: 0, title: "About" },
   { id: 1, title: "General condition" },
@@ -10,9 +14,9 @@ const NAV_MENU_ITEMS = [
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div>
-        <img src="#" alt="" />
-        <div>TipMinder</div>
+      <div className={styles.logo}>
+        <TipMinderIcon />
+        <div className={styles.title}>TipMinder</div>
       </div>
       <nav>
         <ul className={styles.ul}>
@@ -26,9 +30,10 @@ export default function Header() {
         </ul>
       </nav>
       <div>
-        <button type="">
-          <img src="#" alt="" />
-          <div>Log out</div>
+        <button type="button" className={styles.logoutButton}>
+          <UserIcon />
+          <span className={styles.divider} />
+          <span>Log out</span>
         </button>
       </div>
     </header>
