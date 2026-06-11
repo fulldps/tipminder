97bfc038b85055d49f11d68eef07bf46950fcfb6
dbtu <nazarhayidov@gmail.com>
2026-06-11 13:23:32 +0300

    feat(details): add current price area chart using recharts

--- src/components/Details/DetailsChart/DetailsChart.jsx
+++ src/components/Details/DetailsChart/DetailsChart.jsx
@@ -1,4 +1,77 @@
+import { AreaChart, CartesianGrid, Area, YAxis } from "recharts";
+
 import * as styles from "./DetailsChart.module.css";
+
+const DAYS_DATA = [
+  {
+    date: "07/06",
+    amount: 0.6,
+  },
+  {
+    date: "07/09",
+    amount: 0.25,
+  },
+  {
+    date: "01/06",
+    amount: 0.19,
+  },
+  {
+    date: "11/12",
+    amount: 0.62,
+  },
+  {
+    date: "02/08",
+    amount: 0.67,
+  },
+  {
+    date: "04/18",
+    amount: 0.9,
+  },
+  {
+    date: "01/16",
+    amount: 0.6,
+  },
+  {
+    date: "01/16",
+    amount: 0.2,
+  },
+  {
+    date: "01/16",
+    amount: 0.7,
+  },
+  {
+    date: "01/16",
+    amount: 0.2,
+  },
+  {
+    date: "01/16",
+    amount: 0.9,
+  },
+  {
+    date: "01/16",
+    amount: 0.3,
+  },
+  {
+    date: "01/16",
+    amount: 0.1,
+  },
+  {
+    date: "01/16",
+    amount: 0.4,
+  },
+  {
+    date: "01/16",
+    amount: 0.1,
+  },
+  {
+    date: "01/16",
+    amount: 0.6,
+  },
+  {
+    date: "01/16",
+    amount: 0.3,
+  },
+];
 
 export default function DetailsChart() {
   return (
@@ -10,7 +83,13 @@ export default function DetailsChart() {
           <button type="button">Month</button>
         </div>
       </div>
-      <div className={styles.chartBody}></div>
+      <div className={styles.chartBody}>
+        <AreaChart className={styles.lineChart} responsive data={DAYS_DATA}>
+          <CartesianGrid vertical={false} />
+          <YAxis width="auto" />
+          <Area type="monotone" dataKey="amount" />
+        </AreaChart>
+      </div>
     </div>
   );
 }
